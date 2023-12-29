d127f7df549cb677b7ab26cd564d9b52acdd7438
bunbunnibun <harleyputra778@gmail.com>
2023-12-29 15:13:18 +0700

    +History

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,29 +1,43 @@
 // pages/HomePage.tsx
 
 import React, { useState } from 'react';
+import * as moment from 'moment';
 import BalanceTracker from './BalanceTracker';
 import './home.css';
 
+const currentDate: Date = new Date();
+
+const formatter1 = new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+
+const formattedDate1: string = formatter1.format(currentDate);
+
 const HomePage: React.FC = () => {
   const [balance, setBalance] = useState<number>(1000);
+  const [transactionName, setTransactionName] = useState<string>('');
   const [transactionAmount, setTransactionAmount] = useState<string>('');
+  const [transactionDate] = useState<string>(formattedDate1);
   const [transactionHistory, setTransactionHistory] = useState<{
     amount: string;
+    name: string;
+    date: string;
     isPositive: boolean;
   }[]>([]);
 
   const handleAddTransaction = () => {
     const amount = parseFloat(transactionAmount);
+    const name = transactionName;
+    const date = transactionDate;
 
     if (!isNaN(amount)) {
       const isPositive = amount > 0;
 
       setBalance((prevBalance) => prevBalance + amount);
       setTransactionHistory((prevHistory) => [
-        { amount: `${isPositive ? '+' : '-'}$${Math.abs(amount)}`, isPositive },
+        { amount: `${isPositive ? '+' : '-'}$${Math.abs(amount)}`, isPositive, name, date },
         ...prevHistory,
       ]);
       setTransactionAmount('');
+      setTransactionName('');
     }
   };
 
@@ -32,15 +46,16 @@ const HomePage: React.FC = () => {
       <h1>Welcome to One to Many!</h1>
       <BalanceTracker balance={balance} />
       <div className="transactionSection">
-        <h2>My Transactions:</h2>
-        <ul className='history'>
-          {transactionHistory.map((transaction, index) => (
-            <li key={index} style={{ color: transaction.isPositive ? 'green' : 'red' }}>
-              {transaction.amount}
-            </li>
-          ))}
-        </ul>
-        <div className='transactions'>
+        <h2>Log your transaction!</h2>
+        <div className='transactions-name'>
+          <input
+            type="text"
+            placeholder="Enter transaction source"
+            value={transactionName}
+            onChange={(e) => setTransactionName(e.target.value)}
+          />
+        </div>
+        <div className='transactions-amount'>
           <input
             type="number"
             placeholder="Enter amount (positive or negative)"
@@ -49,6 +64,23 @@ const HomePage: React.FC = () => {
           />
           <button onClick={handleAddTransaction}>Add Transaction</button>
         </div>
+        <h3>My Transactions:</h3>
+        <ul className='history'>
+          {transactionHistory.map((transaction, index) => (
+            <li className='history-content' key={index}>
+  {/* style={{ color: transaction.isPositive ? 'green' : 'red' }} */}
+              <div className='content-name'>
+                {transaction.name}
+              </div>
+              <div className='content-date'>
+                {transaction.date}
+              </div>
+              <div className='content-amount'>
+                {transaction.amount}
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
