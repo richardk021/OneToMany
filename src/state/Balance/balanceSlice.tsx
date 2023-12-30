faa2480123b95491869b8e6ab427ed9ada3213b1
bunbunnibun <harleyputra778@gmail.com>
2023-12-30 19:05:43 +0700

    +merged3

---
+++ src/state/Balance/balanceSlice.tsx
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface BalanceState {
+  value: number;
+}
+
+const initialState: BalanceState = {
+  value: 0,
+};
+
+const balanceSlice = createSlice({
+  name: "balance",
+  initialState,
+  reducers: {
+    setBalance: (state, action) => {
+      state.value = action.payload;
+    },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload;
+    },
+    decrementByAmount: (state, action) => {
+      state.value -= action.payload;
+    },
+  },
+});
+
+export const { setBalance, incrementByAmount, decrementByAmount } =
+  balanceSlice.actions;
+
+export default balanceSlice.reducer;
